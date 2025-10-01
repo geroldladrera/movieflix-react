08b71e1cabbbb0c79843500ed0514cb059701574
geroldladrera <geroldladrera@gmail.com>
2025-10-01 22:37:55 +0800

    Add banner image and title

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./hero.png" alt="Hero" />
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              Without the Hassle
+            </h1>
+          </header>
+
+          <p>Search</p>
+        </div>
+      </div>
+    </main>
   );
 };
 
